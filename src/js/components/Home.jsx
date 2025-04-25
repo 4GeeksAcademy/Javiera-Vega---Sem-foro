f2d0bad7389e07386fb3b7b19a6dd03633975ac5
Javiera Vega <javieravegareyes7@gmail.com>
2025-04-25 23:27:35 +0000

    semaforo

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,31 @@
 import React from "react";
+import { useState } from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	const [luz, setLuz] = useState("alumbrado");
+
+	const cambioLuz = () => {
+		setLuz(luz === "alumbrado" ? "" : "alumbrado");
+	};
+
+	return (
+		<div className="container">
+			<div className="sujetador"></div>
+			<div className="caja-semaforo">
+				<div className={`circulo-rojo ${luz}`}
+					onClick={cambioLuz}>
+				</div>
+				<div className={`circulo-amarillo ${luz}`}
+					onClick={cambioLuz}
+				></div>
+				<div className={`circulo-verde ${luz}`}
+					onClick={cambioLuz}>
+				</div>
+			</div>
 		</div>
 	);
 };
