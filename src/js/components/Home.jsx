20b3062777bda7f28989c9af6ecce2ea59cf85c5
Javiera Vega <javieravegareyes7@gmail.com>
2025-04-28 19:18:52 +0000

    add circle purple

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,31 +1,40 @@
-import React from "react";
-import { useState } from "react";
-
-//include images into your bundle
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
 
-	const [luz, setLuz] = useState("alumbrado");
+	const [luz, setLuz] = useState("");
+	const [luzPurple, setLuzPurple] = useState(true)
 
-	const cambioLuz = () => {
-		setLuz(luz === "alumbrado" ? "" : "alumbrado");
+	const cambioLuz = (color) => {
+		setLuz(color);
 	};
 
 	return (
 		<div className="container">
-			<div className="sujetador"></div>
-			<div className="caja-semaforo">
-				<div className={`circulo-rojo ${luz}`}
-					onClick={cambioLuz}>
+			<div className="bra-black"></div>
+			<div className="box-traffic-light">
+				<div className={`circle-red ${luz == "red" ? "alumbrado" : ""}`}
+					onClick={() => cambioLuz("red")}>
 				</div>
-				<div className={`circulo-amarillo ${luz}`}
-					onClick={cambioLuz}
+				<div className={`circle-yellow ${luz == "yellow" ? "alumbrado" : ""}`}
+					onClick={() => cambioLuz("yellow")}
 				></div>
-				<div className={`circulo-verde ${luz}`}
-					onClick={cambioLuz}>
+				<div className={`circle-green ${luz == "green" ? "alumbrado" : ""}`}
+					onClick={() => cambioLuz("green")}>
 				</div>
+
+				{ luzPurple ?
+					<div className={`circle-purple ${luz == "purple" ? "alumbrado" : ""}`}
+					onClick={() => cambioLuz("purple")}>
+				</div> : ""}
 			</div>
+			<div className="mt-5">
+			<button className="btn btn-dark"
+					onClick={() => setLuzPurple(!luzPurple)}>
+					Add color purple</button>
+			</div>
+		
 		</div>
 	);
 };
